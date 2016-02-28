dc7ea38969228b55cea93307999846318ea99058
Alex Ristich <alexristich@gmail.com>
2016-02-27 19:57:04 -0800

    Implementation of parsing for earthquake data

--- both/Quakes.jsx
+++ both/Quakes.jsx
@@ -3,28 +3,35 @@ Quakes = new Mongo.Collection('quakes');
 var Schemas = {};
 
 Schemas.Quake = new SimpleSchema({
+    id: {
+        type: String,
+        label: "Solution ID"
+    },
+
     date: {
-        type: Date,
-        label: "Date"
+        // TODO change type back to Date after converting data to Date object type
+        type: String,
+        label: "Origin Time",
+        optional: true
     },
 
     lat: {
-        type: Number,
+        type: Float,
         label: "Latitude"
     },
 
     lon: {
-        type: Number,
+        type: String,
         label: "Longitude"
     },
 
     mag: {
-        type: Number,
+        type: String,
         label: "Magnitude"
     },
 
     depth: {
-        type: Number,
+        type: String,
         label: "Earthquake Depth",
         optional: true
     },
@@ -36,7 +43,7 @@ Schemas.Quake = new SimpleSchema({
     },
 
     momag: {
-        type: Number,
+        type: String,
         label: "Moment Magnitude",
         optional: true
     }
@@ -48,14 +55,45 @@ Quakes.attachSchema(Schemas.Quake);
 Meteor.methods({
     // this contains all the methods to store and retrieve earthquake data
     getEQData: function() {
-        // TODO: create JSON file
-        HTTP.get(Meteor.absoluteUrl("earthquakes.JSON"), function(error, result){
-            Meteor.call("parseEQData", result.data)
+        // TODO: figure out how to collate data from multiple sources
+        HTTP.get("http://www.earthquakescanada.nrcan.gc.ca/api/earthquakes/latest/7d.json",
+            function(error, result){
+                Meteor.call("parseEQData", result.data);
         });
     },
 
     parseEQData: function(eqData) {
-        //TODO: figure out logic to parse the data
+        var quakeName = {};
+        for (var quake in eqData) {
+            if (quake !== "metadata") {
+                quakeName = quake;
+                Meteor.call("addEarthquake", eqData[quakeName]);
+            }
+        }
+
+
+        console.log(Quakes.find({}).fetch())
+    },
+
+    addEarthquake: function(quakeData) {
+        // TODO add quake
+        // TODO parse location
+
+        var quake = {
+            id: quakeData.solution_id,
+            // TODO set this as a JS Date object
+            date: quakeData.origin_time,
+            lat: quakeData.geoJSON.coordinates[0],
+            lon: quakeData.geoJSON.coordinates[1],
+            mag: quakeData.magnitude,
+            depth: quakeData.depth,
+            type: quakeData.magnitude_type
+        };
+
+        console.log(quake);
+
+        // TODO check if we need to change this to upsert when adding additional data sets
+        Quakes.insert(quake);
     }
 
 });
